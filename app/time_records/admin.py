aaf08cef27c95d481eababb567c03dfa402a0c39
always-awake <leemirim9654@gmail.com>
2019-11-23 23:57:55 +0900

    [ IMPROVE ] time_records 모델 구현
    - creator 필드와 daliy_record 필드를 갖는 TimeRecord 모델 구현
    - creator와 TimeRecord는 1:n 관계
    - DailyRecord와 TimeRecord는 1:n 관계

---
+++ app/time_records/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from . import models
+
+
+@admin.register(models.TimeRecord)
+class TimeRecordAdmin(admin.ModelAdmin):
+    list_display = (
+      'id',
+      'creator',
+      'created_at',
+    )
---
+++ app/time_records/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.conf import settings
+from daily_records.models import TimeStampedModel, DailyRecord
+
+
+class TimeRecord(TimeStampedModel):
+    creator = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        null=True,
+        related_name='time_records'
+    )
+    daily_record = models.ForeignKey(
+      DailyRecord,
+      on_delete=models.CASCADE,
+      null=True,
+      related_name='time_records'
+    )
+
+    def __str__(self):
+        return f'Time Record - {self.creator}'
