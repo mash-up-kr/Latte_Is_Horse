66a2aded30278aef8ef20cf1195d95c694054433
kimkyunghun3 <kimkyunghun3@naver.com>
2019-11-30 13:53:04 +0900

    [FIX] User모델 수정
    - ModelAdmin을 UserAdmin으로 수정
    - image_profile 모델 추가

--- app/users/admin.py
+++ app/users/admin.py
@@ -5,6 +5,10 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 @admin.register(models.User)
 class UserAdmin(BaseUserAdmin):
+    filedsets = (
+        ('추가필드', {'field': ('img_profile',
+                            )}),
+    )
     list_display = (
         'id',
         'username',
